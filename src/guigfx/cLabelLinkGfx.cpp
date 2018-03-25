2686f007bb073380bf8392f4c2e9db979d4b3c0f
aaaasmile <testhaus@gmx.at>
2018-03-25 21:04:57 +0200

    cLabelLinkGfx compile

--- src/guigfx/cLabelLinkGfx.cpp
+++ src/guigfx/cLabelLinkGfx.cpp
@@ -1,27 +1,3 @@
-/*
-    Tressette
-    Copyright (C) 2005  Igor Sarzi Sartori
-
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Library General Public
-    License as published by the Free Software Foundation; either
-    version 2 of the License, or (at your option) any later version.
-
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Library General Public License for more details.
-
-    You should have received a copy of the GNU Library General Public
-    License along with this library; if not, write to the Free
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
-    Igor Sarzi Sartori
-    www.invido.it
-    6colpiunbucosolo@gmx.net
-*/
-
-
 
 // cLabelLinkGfx.cpp: implementation of the cLabelLinkGfx class.
 //
@@ -41,20 +17,20 @@
 
 cLabelLinkGfx::cLabelLinkGfx()
 {
-    m_eState = INVISIBLE;
-    m_pFontText = 0;
-    m_iButID = 0;
-    m_bIsEnabled = TRUE;
-    m_pSurf_Bar = 0;
+	m_eState = INVISIBLE;
+	m_pFontText = 0;
+	m_iButID = 0;
+	m_bIsEnabled = TRUE;
+	m_pSurf_Bar = 0;
 }
 
 cLabelLinkGfx::~cLabelLinkGfx()
 {
-    if (m_pSurf_Bar)
-    {
-        SDL_FreeSurface(m_pSurf_Bar);
-        m_pSurf_Bar = NULL;
-    }
+	if (m_pSurf_Bar)
+	{
+		SDL_FreeSurface(m_pSurf_Bar);
+		m_pSurf_Bar = NULL;
+	}
 }
 
 
@@ -63,148 +39,151 @@ cLabelLinkGfx::~cLabelLinkGfx()
 ////////////////////////////////////////
 //       Init
 /*! Init the button
-// \param SDL_Rect* pRect : 
-// \param SDL_Surface*  pScreen : 
+// \param SDL_Rect* pRect :
+// \param SDL_Surface*  pScreen :
 */
-void  cLabelLinkGfx::Init(SDL_Rect* pRect, SDL_Surface*  pScreen, TTF_Font* pFont, int iButID)
+void  cLabelLinkGfx::Init(SDL_Rect* pRect, SDL_Surface*  pScreen, TTF_Font* pFont, int iButID, SDL_Renderer* psdlRenderer)
 {
-    m_rctButt = *pRect;
+	m_rctButt = *pRect;
+	m_psdlRenderer = psdlRenderer;
 
-    // black bar surface
-    m_pSurf_Bar = SDL_CreateRGBSurface(SDL_SWSURFACE, m_rctButt.w, m_rctButt.h, 32, 0, 0, 0, 0);
-    SDL_FillRect(m_pSurf_Bar, NULL, SDL_MapRGBA(pScreen->format, 255, 0, 0, 0));
-    SDL_SetAlpha(m_pSurf_Bar, SDL_SRCALPHA, 127);
-    m_pFontText = pFont;
+	// black bar surface
+	m_pSurf_Bar = SDL_CreateRGBSurface(SDL_SWSURFACE, m_rctButt.w, m_rctButt.h, 32, 0, 0, 0, 0);
+	SDL_FillRect(m_pSurf_Bar, NULL, SDL_MapRGBA(pScreen->format, 255, 0, 0, 0));
+	//SDL_SetAlpha(m_pSurf_Bar, SDL_SRCALPHA, 127); //SDL 1.2
+	SDL_SetSurfaceAlphaMod(m_pSurf_Bar, 127); // SDL 2.0
 
-    m_colCurrent = GFX_UTIL_COLOR::White;
-    m_iButID = iButID;
+	m_pFontText = pFont;
+
+	m_colCurrent = GFX_UTIL_COLOR::White;
+	m_iButID = iButID;
 
 }
 
 
 ////////////////////////////////////////
 //       SetState
-/*! 
-// \param eSate eVal : 
+/*!
+// \param eSate eVal :
 */
 void   cLabelLinkGfx::SetState(eSate eVal)
 {
-    eSate eOldState = m_eState;
-    m_eState = eVal;
-    if (eOldState != m_eState &&
-        m_eState == VISIBLE)
-    {
-        m_colCurrent = GFX_UTIL_COLOR::White;
-    }
+	eSate eOldState = m_eState;
+	m_eState = eVal;
+	if (eOldState != m_eState &&
+		m_eState == VISIBLE)
+	{
+		m_colCurrent = GFX_UTIL_COLOR::White;
+	}
 }
 
 ////////////////////////////////////////
 //       MouseMove
-/*! 
-// \param SDL_Event &event : 
+/*!
+// \param SDL_Event &event :
 */
-void   cLabelLinkGfx::MouseMove(SDL_Event &event, SDL_Surface* pScreen, SDL_Surface* pScene_background)
+void   cLabelLinkGfx::MouseMove(SDL_Event &event, SDL_Surface* pScreen, SDL_Texture* pScene_background, SDL_Texture* pScreenTexture)
 {
-    if (m_eState == VISIBLE && m_bIsEnabled)
-    {
-        if ( event.motion.x >=  m_rctButt.x  && event.motion.x <= m_rctButt.x + m_rctButt.w &&
-             event.motion.y >=  m_rctButt.y  && event.motion.y <= m_rctButt.y + m_rctButt.h)
-        {
-            // mouse inner button
-            m_colCurrent = GFX_UTIL_COLOR::Orange;
-            Redraw(pScreen, pScene_background);
-        }
-        else
-        {
-            // mouse outside
-            if (m_colCurrent.r == GFX_UTIL_COLOR::Orange.r &&
-                m_colCurrent.g == GFX_UTIL_COLOR::Orange.g &&
-                m_colCurrent.b == GFX_UTIL_COLOR::Orange.b)
-            {
-                // button was selected
-                m_colCurrent = GFX_UTIL_COLOR::White;
-                Redraw(pScreen, pScene_background);
-            }
-        }
-    }
+	if (m_eState == VISIBLE && m_bIsEnabled)
+	{
+		if (event.motion.x >= m_rctButt.x  && event.motion.x <= m_rctButt.x + m_rctButt.w &&
+			event.motion.y >= m_rctButt.y  && event.motion.y <= m_rctButt.y + m_rctButt.h)
+		{
+			// mouse inner button
+			m_colCurrent = GFX_UTIL_COLOR::Orange;
+			Redraw(pScreen, pScene_background, pScreenTexture);
+		}
+		else
+		{
+			// mouse outside
+			if (m_colCurrent.r == GFX_UTIL_COLOR::Orange.r &&
+				m_colCurrent.g == GFX_UTIL_COLOR::Orange.g &&
+				m_colCurrent.b == GFX_UTIL_COLOR::Orange.b)
+			{
+				// button was selected
+				m_colCurrent = GFX_UTIL_COLOR::White;
+				Redraw(pScreen, pScene_background, pScreenTexture);
+			}
+		}
+	}
 }
 
 
 ////////////////////////////////////////
 //       MouseUp
 /*! Mouse up event
-// \param SDL_Event &event : 
+// \param SDL_Event &event :
 */
 void   cLabelLinkGfx::MouseUp(SDL_Event &event)
 {
-    if (m_eState == VISIBLE && m_bIsEnabled)
-    {
-        if ( event.motion.x >=  m_rctButt.x  && event.motion.x <= m_rctButt.x + m_rctButt.w &&
-             event.motion.y >=  m_rctButt.y  && event.motion.y <= m_rctButt.y + m_rctButt.h)
-        {
-            // mouse go up inner the button
-            // send a click event 
-             m_fncbClickEvent( m_iButID);
-             if (m_strUrl.length())
-             {
+	if (m_eState == VISIBLE && m_bIsEnabled)
+	{
+		if (event.motion.x >= m_rctButt.x  && event.motion.x <= m_rctButt.x + m_rctButt.w &&
+			event.motion.y >= m_rctButt.y  && event.motion.y <= m_rctButt.y + m_rctButt.h)
+		{
+			// mouse go up inner the button
+			// send a click event 
+			m_fncbClickEvent(m_iButID);
+			if (m_strUrl.length())
+			{
 #ifdef WIN32
-                 ShellExecute(0, "open", m_strUrl.c_str() , 0, 0, SW_SHOWNORMAL);	
+				ShellExecute(0, "open", m_strUrl.c_str(), 0, 0, SW_SHOWNORMAL);
 #endif
-             }
-        }
-    }
+			}
+		}
+	}
 }
 
 
 ////////////////////////////////////////
 //       Draw
 /*! Draw the label with text
-// \param SDL_Surface*  pScreen : 
+// \param SDL_Surface*  pScreen :
 */
 void   cLabelLinkGfx::Draw(SDL_Surface*  pScreen)
 {
-    if (m_eState != INVISIBLE)
-    {
-        if (m_bIsEnabled)
-        {
-            // begin stuff mouse
-            Uint8 state;
-            int mx, my;
-            state = SDL_GetMouseState(&mx, &my);
-            if ( mx >=  m_rctButt.x  && mx <= m_rctButt.x + m_rctButt.w &&
-                  my >=  m_rctButt.y  && my <= m_rctButt.y + m_rctButt.h)
-            {
-                // mouse on button
-                m_colCurrent = GFX_UTIL_COLOR::Orange;
-            }
-            else
-            {
-                m_colCurrent = GFX_UTIL_COLOR::White;
-            }
-            // end stuff mouse
+	if (m_eState != INVISIBLE)
+	{
+		if (m_bIsEnabled)
+		{
+			// begin stuff mouse
+			Uint8 state;
+			int mx, my;
+			state = SDL_GetMouseState(&mx, &my);
+			if (mx >= m_rctButt.x  && mx <= m_rctButt.x + m_rctButt.w &&
+				my >= m_rctButt.y  && my <= m_rctButt.y + m_rctButt.h)
+			{
+				// mouse on button
+				m_colCurrent = GFX_UTIL_COLOR::Orange;
+			}
+			else
+			{
+				m_colCurrent = GFX_UTIL_COLOR::White;
+			}
+			// end stuff mouse
 
-            int tx, ty;
-	        TTF_SizeText(m_pFontText, m_strButText.c_str(), &tx, &ty);
-            int iXOffSet = (m_rctButt.w - tx)/2 ;
-            if (iXOffSet < 0)
-            {
-                iXOffSet = 1;
-            }
-            int iYOffset = (m_rctButt.h - ty)/2 ;
+			int tx, ty;
+			TTF_SizeText(m_pFontText, m_strButText.c_str(), &tx, &ty);
+			int iXOffSet = (m_rctButt.w - tx) / 2;
+			if (iXOffSet < 0)
+			{
+				iXOffSet = 1;
+			}
+			int iYOffset = (m_rctButt.h - ty) / 2;
 
-            iYOffset = 0;
-            iXOffSet = 0;
-            GFX_UTIL::DrawString(pScreen, m_strButText.c_str(), m_rctButt.x + iXOffSet, 
-                                 m_rctButt.y + iYOffset, m_colCurrent, m_pFontText);
+			iYOffset = 0;
+			iXOffSet = 0;
+			GFX_UTIL::DrawString(pScreen, m_strButText.c_str(), m_rctButt.x + iXOffSet,
+				m_rctButt.y + iYOffset, m_colCurrent, m_pFontText);
 
-            
-        }
-        else
-        {
-            // button disabled 
-            // TO DO
-        }
-    }
+
+		}
+		else
+		{
+			// button disabled 
+			// TO DO
+		}
+	}
 }
 
 
@@ -212,15 +191,20 @@ void   cLabelLinkGfx::Draw(SDL_Surface*  pScreen)
 ////////////////////////////////////////
 //       Redraw
 /*! Redraw the button
-// \param SDL_Surface* pScreen : 
-// \param SDL_Surface* pScene_background : 
+// \param SDL_Surface* pScreen :
+// \param SDL_Surface* pScene_background :
 */
-void   cLabelLinkGfx::Redraw(SDL_Surface* pScreen, SDL_Surface* pScene_background)
+void cLabelLinkGfx::Redraw(SDL_Surface* pScreen, SDL_Texture* pScene_background, SDL_Texture* pScreenTexture)
 {
-    if (pScene_background)
-    {
-        SDL_BlitSurface(pScene_background, &m_rctButt, pScreen, &m_rctButt);
-    }
-    Draw(pScreen);
-    SDL_Flip(pScreen);
+	if (pScene_background)
+	{
+		//SDL_BlitSurface(pScene_background, &m_rctButt, pScreen, &m_rctButt); //SDL 1.2
+		SDL_RenderCopy(m_psdlRenderer, pScene_background, &m_rctButt, &m_rctButt); //SDL 2.0
+	}
+	Draw(pScreen);
+	//SDL_Flip(pScreen);
+	// SDL 2.0
+	SDL_UpdateTexture(pScreenTexture, NULL, pScreen->pixels, pScreen->pitch);
+	SDL_RenderCopy(m_psdlRenderer, pScreenTexture, NULL, NULL);
+	SDL_RenderPresent(m_psdlRenderer);
 }
--- src/guigfx/cLabelLinkGfx.h
+++ src/guigfx/cLabelLinkGfx.h
@@ -1,26 +1,3 @@
-/*
-    Tressette
-    Copyright (C) 2005  Igor Sarzi Sartori
-
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Library General Public
-    License as published by the Free Software Foundation; either
-    version 2 of the License, or (at your option) any later version.
-
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Library General Public License for more details.
-
-    You should have received a copy of the GNU Library General Public
-    License along with this library; if not, write to the Free
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
-    Igor Sarzi Sartori
-    www.invido.it
-    6colpiunbucosolo@gmx.net
-*/
-
 
 // cLabelLinkGfx.h: interface for the cLabelLinkGfx class.
 //
@@ -40,9 +17,8 @@
 #include "FastDelegate.h"
 using namespace fastdelegate;
 
-
 //! class  cLabelLinkGfx  
-class DLL_EXPORTIMPORT cLabelLinkGfx  
+class cLabelLinkGfx  
 {
     // use fastdelegate to implement the observer pattern and avoid using the callback interface on observer
     typedef FastDelegate1<int> CLICKEVENT;
@@ -60,11 +36,11 @@ public:
     virtual ~cLabelLinkGfx();
 
     //! init the control 
-    void   Init(SDL_Rect* pRect, SDL_Surface*  pScreen, TTF_Font* pFont, int iButID);
+    void   Init(SDL_Rect* pRect, SDL_Surface*  pScreen, TTF_Font* pFont, int iButID, SDL_Renderer* psdlRenderer);
     //! set window text
     void   SetWindowText(LPCSTR strCaption){m_strButText = strCaption;}
     //! mouse move event
-    void   MouseMove(SDL_Event &event, SDL_Surface* pScreen, SDL_Surface* pScene_background);
+    void   MouseMove(SDL_Event &event, SDL_Surface* pScreen, SDL_Texture* pScene_background, SDL_Texture* pScreenTexture);
     //! mouse up
     void   MouseUp(SDL_Event &event);
     //! draw the button 
@@ -74,7 +50,7 @@ public:
     //! set button state
     void   SetState(eSate eVal);
     //! redraw the button
-    void   Redraw(SDL_Surface* pScreen, SDL_Surface* pScene_background);
+    void   Redraw(SDL_Surface* pScreen, SDL_Texture* pScene_background, SDL_Texture* pScreenTexture);
     //! set the url
     void   SetUrl(LPCSTR lpszUrl){m_strUrl = lpszUrl;}
 
@@ -101,8 +77,7 @@ protected:
     int             m_iButID;
     //! url
     STRING          m_strUrl;
-    
-
+	SDL_Renderer*   m_psdlRenderer;
 };
 
 #endif // !defined(AFX_cLabelLinkGfx_H__2F3F455A_3EA3_49D6_84D9_A7D4188985C3__INCLUDED_)
